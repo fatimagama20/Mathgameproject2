ea0c39c1bb0af3814c46cc287044b65a145ffd29
fatimagama20 <familygama20@gmail.com>
2023-07-04 22:14:39 +0000

    Set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function startGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function generateWrongAnswers(){
+
+}
+function displaymultiplyQuestions(){
+    
+}
