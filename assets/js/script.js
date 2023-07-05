13ecc6fb6570057caa61ee1a74a62d7a49180f8f
fatimagama20 <familygama20@gmail.com>
2023-07-05 22:17:49 +0000

    Start to write startgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-function startGame(){
-
+//wait for the DOM to finish loading before running the game
+//Get the button elements and add even listeners to them
+document.getElementById('startReset').onclick = function(){
+    location.reload()
+    startGame();
 }
+function startGame(){
+    //create tow random numbers between 1 and 10
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+let product = num1 * num2;
+document.getElementById("question").innerHTML = num1 + '&times;' + num2;
+}
+
 function checkAnswer(){
 
 }
@@ -14,5 +25,5 @@ function generateWrongAnswers(){
 
 }
 function displaymultiplyQuestions(){
-    
+
 }
