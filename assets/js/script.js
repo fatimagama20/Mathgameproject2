74a22c1711f173bc30e7a38baeb7b6a6b573c473
fatimagama20 <familygama20@gmail.com>
2023-07-09 19:27:44 +0000

    Add displayquestions and settimeinterval functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 //wait for the DOM to finish loading before running the game
 //Get the button elements and add even listeners to them
+let timeremaining;
 document.getElementById('startReset').onclick = function(){
-    location.reload()
+ //   location.reload()
+    timeremaining=60;
+    settimeinterval();
     startGame();
 }
 function startGame(){
@@ -9,7 +12,7 @@ function startGame(){
 let num1 = Math.floor(Math.random() * 10) + 1;
 let num2 = Math.floor(Math.random() * 10) + 1;
 let product = num1 * num2;
-document.getElementById("question").innerHTML = num1 + '&times;' + num2;
+displaymultiplyQuestions(num1,num2);
 }
 
 function checkAnswer(){
@@ -24,6 +27,27 @@ function incrementScore(){
 function generateWrongAnswers(){
 
 }
-function displaymultiplyQuestions(){
-
+function displaymultiplyQuestions(num1,num2){
+    document.getElementById("question").innerHTML = num1 + '&times;' + num2;
 }
+function settimeinterval(){
+    let settimeinterval = setInterval(function(){
+      // if the time value is greater than zero
+      if(timeremaining>0){
+        timeremaining--;
+      document.getElementById('remainingsecs').innerHTML=timeremaining;
+      }else{
+        //if time value is zero
+        stoptimeinterval()
+       // alert("GAME IS OVER")
+       // show("gameover")
+        document.getElementById('startReset').innerHTML = "Start Game"
+      //  document.getElementById("gameover").innerHTML = "<p> GAME IS OVER</p><br><p>YOUR SCORE IS "+score +"</p>"
+        playing = false
+      }
+    },1000)
+  }
+
+  function stoptimeinterval(){
+    clearInterval(settimeinterval)
+  }
