238a6687ffb8e07364602ac75fe271a1b83527af
fatimagama20 <familygama20@gmail.com>
2023-07-10 19:53:27 +0000

    Add displaycorrectanswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,48 @@
 //wait for the DOM to finish loading before running the game
 //Get the button elements and add even listeners to them
+
+let playing = false;
 let timeremaining;
+let product;
 document.getElementById('startReset').onclick = function(){
- //   location.reload()
-    timeremaining=60;
-    settimeinterval();
-    startGame();
+ if (playing == true){
+  location.reload()
+ } else{
+  playing = true;
+  hide("gameover");
+  show("timeremaining");
+  document.getElementById('startReset').innerHTML = "Reset Game";
+  score=0;
+  document.getElementById('scorevalue').innerHTML= score;
+  timeremaining=30;
+  settimeinterval();
+  startGame();
+  calculateCorrectAnswer();
+ } 
 }
 function startGame(){
     //create tow random numbers between 1 and 10
 let num1 = Math.floor(Math.random() * 10) + 1;
 let num2 = Math.floor(Math.random() * 10) + 1;
-let product = num1 * num2;
-displaymultiplyQuestions(num1,num2);
+product = num1 * num2;
+document.getElementById("question").innerHTML = num1 + '&times;' + num2;
 }
 
 function checkAnswer(){
 
 }
 function calculateCorrectAnswer(){
-
+  let choiceRandom = Math.round(Math.random()*2) + 1;
+  document.getElementById('box'+choiceRandom).innerHTML = product;
 }
+
 function incrementScore(){
 
 }
 function generateWrongAnswers(){
 
 }
-function displaymultiplyQuestions(num1,num2){
-    document.getElementById("question").innerHTML = num1 + '&times;' + num2;
-}
+
 function settimeinterval(){
     let settimeinterval = setInterval(function(){
       // if the time value is greater than zero
@@ -40,7 +53,7 @@ function settimeinterval(){
         //if time value is zero
         stoptimeinterval()
        // alert("GAME IS OVER")
-       // show("gameover")
+       show("gameover")
         document.getElementById('startReset').innerHTML = "Start Game"
       //  document.getElementById("gameover").innerHTML = "<p> GAME IS OVER</p><br><p>YOUR SCORE IS "+score +"</p>"
         playing = false
@@ -50,4 +63,11 @@ function settimeinterval(){
 
   function stoptimeinterval(){
     clearInterval(settimeinterval)
+  }
+ function show(id){
+    document.getElementById(id).style.display="block"
+  }
+  // to hide an html element
+  function hide(id){
+    document.getElementById(id).style.display="none"
   }
